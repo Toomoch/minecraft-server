1325b69cf012f2e0fb7e40b8ff383a0005b6ba61
Toomoch <14185777+Toomoch@users.noreply.github.com>
2021-07-14 18:47:21 +0200

    Update dobackup.sh

--- dobackup.sh
+++ dobackup.sh
@@ -4,7 +4,7 @@ localbackupdir=backup
 serverdir=server
 rconname=localhost:25575:minecraft
 
-./minecraft-backup/backup.sh -c -i $serverdir -o $localbackupdir -s $rconname -w con -m 15 -d sequential
+./minecraft-backup/backup.sh -c -i $serverdir -o $localbackupdir -s $rconname -w rcon -m 15 -d sequential
 
 logfile=rclonelog.txt
 rcloneremote1=upc
